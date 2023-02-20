896b66a3674202a4d7b2eaa16e136c7341059501
trungson1 <luongtrungson01@gmail.com>
2023-02-20 15:28:12 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Bai_thuc_Hanh.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
